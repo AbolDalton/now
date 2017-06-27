a388c6272e36fc62f637613c49f2219c4c79ee05
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2017-06-27 17:01:50 +0430

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -163,7 +163,7 @@ function run(msg, matches)
 	end
 end
 return {
-advan = "http://github.com/janlou/AdvanSource",
+dalad = "seed edited",
 patterns = {"^[!#/](help)$","^[!#/]([Ss]ethelp) (.*)$","^[!#/](delhelp)$"},
 run = run,
 }
