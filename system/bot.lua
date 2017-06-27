40da73f61e0366b3d9df8a7d403ebb5c1c812538
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2017-06-27 16:45:52 +0430

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -232,19 +232,18 @@ function create_config( )
     "tools"
     },
     vip_users = {}, --vip users
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
+    sudo_users = {368305537,tonumber(our_id)},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
-This is an original bot and based on (AdvanSource).
+This is an original bot and based on (DALAD Soures).
 Copyright all right reserved and you must respect all laws.
 
-Source: https://github.com/janlou/AdvanSource
-Channel: @AdvanTeam
-Messenger: @Advanbot
-Creator: @janlou
-Site: http://StoreVps.net
-Version: [4.1]
+Source:Seed Edited
+Channel: @The_Dalton
+Messenger: @Abol_Myth_Dalton
+Creator: @Abol_Myth_Dalton
+Version: Test For Beta
 ]],
     --Start rate:
     Group_rate = [[]],
